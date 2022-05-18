20cd3fdd124a52aa3673cf9dae4aeb9da1c0130f
Martin Blanco <martineduardoblanco@hotmail.com>
2022-05-18 10:52:03 -0300

    Aplicando modificaciones para la entrega del proyecto final

--- funciones.js
+++ funciones.js
@@ -6,10 +6,25 @@ async function cargaProductos () {
     
 }
 
+// funcion que actualiza la cantidad de los productos
+function actualizar (carrito){
+    let cantidad=0
+    for(vino of carrito){
+        if(vino.cantidad>0){
+            cantidad+= vino.cantidad
+        }
+    }
+    if(cantidad>0){
+       document.getElementById("total-carrito").innerHTML=cantidad
+    }
+    
+}
 
-// Al hacer click al boton Agregar carrito Muestra los productos en una tabla dentro del carrito
+// Funcion clickBoton que muestra los productos agregados al carrito
 function clickBoton(productos, i) {
-    productos[i].cantidad++;
+    const cantidad = document.getElementById(`cant-${i}`)
+    productos[i].cantidad = productos[i].cantidad + parseInt(cantidad.value);
+    sumarTotalCarro(parseInt(cantidad.value))
     localStorage.setItem("compra", JSON.stringify(productos))
     Toastify({
         text: "Producto Cargado!",
@@ -20,15 +35,33 @@ function clickBoton(productos, i) {
           },
         position: "right"
     }).showToast();
+     cantidad.value = 1
+}
+
+// Funcion que suma la cantidad de los productos en la tabla del Carrito
+function sumarTotalCarro(productosAgregados) {
+    const totalCarrito = document.getElementById('total-carrito').innerHTML
+    const totalAnterior = totalCarrito ? parseInt(totalCarrito) : 0
+    const nuevoTotal = totalAnterior + productosAgregados
+    document.getElementById('total-carrito').innerHTML = nuevoTotal
+}
+
+// Funcion que resta la cantidad de los productos en la tabla del Carrito
+function restarTotalCarro() {
+    let totalCarrito = parseInt(document.getElementById('total-carrito').innerHTML)
+    totalCarrito--
+    totalCarrito==0 ?  document.getElementById('total-carrito').innerHTML="": 
+    document.getElementById('total-carrito').innerHTML = totalCarrito
 }
 
 
-// Elimina de una tabla el producto seleccionado
+// Funcion que elimina de una tabla el producto seleccionado
 function eliminarProducto(productos) {
     productos.forEach((productoSeleccionado, i) => {
         if (productoSeleccionado.cantidad > 0) {
             document.getElementById(`btn${productoSeleccionado.id}`).addEventListener("click", () => {
                 productoSeleccionado.cantidad--;
+                restarTotalCarro()
                 let total = 0;
                 tabla.innerHTML = "";
                 Toastify({
--- script.js
+++ script.js
@@ -4,41 +4,55 @@ const vaciar = document.getElementById("vaciar");
 const btnCarrito = document.getElementById("btnCarrito");
 const comprar = document.getElementById("comprar");
 let divProductos = document.getElementById("productos")
-
 let carrito = []
+//llamado de la funcion que trae los productos desde el JSON
 let vinos = cargaProductos();
 
-vinos.then(data =>{
-    divProductos.innerHTML= "";
-    data.forEach((v,i)=>{
-        divProductos.innerHTML += `<div id="productos${v.id}" class="card mb-3" style="width: 18rem;">
+
+vinos.then(data => {
+    divProductos.innerHTML = "";
+    data.forEach((v, i) => {
+        divProductos.innerHTML += `<div id="productos${v.id}" class="card mb-3 col-md-4 cardProductos" style="width: 18rem;">
         <img src="./imagenes/${v.imagen}" class="card-img-top img-fluid mb-2" alt="${v.nombre}">
-        <div class="card-body">
-            <h5 class="card-title">${v.nombre}</h5>
-            <p class="card-text">Precio: ${v.precio}</p>
-            <a class="btn btn-primary" id="boton${v.id}">Agregar Carrito</a>
+            <div class="card-body">
+                <h5 class="card-title">${v.nombre}</h5>
+                <p class="card-text">Precio: $${v.precio}</p>
+                <div class= "cantidadProductos"> 
+                    <input type="number" id="cant-${v.id}" value=1 min=1 class="colorCant"/>
+                    <a class="btn btn-primary" id="boton${v.id}">Agregar al Carrito</a>
+                </div> 
         </div>
     </div>`
-    carrito[i]= new Carrito(v.id, v.nombre, v.precio, v.imagen)
-    })  
-    localStorage.getItem("compra")? productos = JSON.parse(localStorage.getItem("compra")):localStorage.setItem("compra", JSON.stringify(productos))
-     
+        carrito[i] = new Carrito(v.id, v.nombre, v.precio, v.imagen)
+    })
+    //Trae datos del storage o los carga
+    localStorage.getItem("compra") ? carrito = JSON.parse(localStorage.getItem("compra")) : localStorage.setItem("compra", JSON.stringify(carrito))
+
+    actualizar(carrito);
+
+
+    let boton0 = document.getElementById("boton0");
     let boton1 = document.getElementById("boton1");
     let boton2 = document.getElementById("boton2");
     let boton3 = document.getElementById("boton3");
     let boton4 = document.getElementById("boton4");
     let boton5 = document.getElementById("boton5");
     let boton6 = document.getElementById("boton6");
-    
-    const botones = [boton1, boton2, boton3, boton4, boton5, boton6];
-    
+    let boton7 = document.getElementById("boton7");
+    let boton8 = document.getElementById("boton8");
+    let boton9 = document.getElementById("boton9");
+    let boton10 = document.getElementById("boton10");
+    let boton11 = document.getElementById("boton11");
+
+    const botones = [boton0, boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8, boton9, boton10, boton11];
+
     botones.forEach((btn, i) => {
         btn.onclick = () => { clickBoton(carrito, i) }
     })
 })
 
 
-// Al hacer click al boton vaciar carrito, vacia la lista de productos en una tabla
+// vaciar onclick, vacia la lista de productos del carrito
 vaciar.onclick = () => {
     Swal.fire({
         title: 'Estás seguro de vaciar el carrito?',
@@ -46,7 +60,7 @@ vaciar.onclick = () => {
         showCancelButton: true,
         confirmButtonText: 'Sí, seguro',
         cancelButtonText: 'No, no quiero'
-    }).then((result) => {  
+    }).then((result) => {
         if (result.isConfirmed) {
             Swal.fire({
                 title: 'Carrito vacio!',
@@ -56,32 +70,35 @@ vaciar.onclick = () => {
                 producto.cantidad = 0;
             }
             tabla.innerHTML = "";
+            document.getElementById('total-carrito').innerHTML=""
             mostrarTotal.innerHTML = `El carrito esta vacio`;
             localStorage.setItem("compra", JSON.stringify(carrito))
         }
     })
-    
+
 };
 
-// Al hacer click al boton comprar, se realiza la compra
-comprar.onclick= ()=>{
+
+// comprar onclick, accion que realiza la compra de los productos
+comprar.onclick = () => {
     Swal.fire({
         title: 'Estás seguro de realizar la compra?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonText: 'Sí, seguro',
         cancelButtonText: 'No, no quiero'
-    }).then((result) => {  
+    }).then((result) => {
         if (result.isConfirmed) {
             Swal.fire({
                 title: 'Compra realizada!',
                 icon: 'success',
-                text: 'La compra ha sido realizada con exito!'
+                text: 'La compra se ha realizado con exito!'
             })
             for (producto of carrito) {
                 producto.cantidad = 0;
             }
             tabla.innerHTML = "";
+            document.getElementById('total-carrito').innerHTML=""
             mostrarTotal.innerHTML = `El carrito esta vacio`;
             localStorage.setItem("compra", JSON.stringify(carrito))
         }
@@ -93,7 +110,7 @@ btnCarrito.addEventListener("click", () => {
     let total = 0;
     tabla.innerHTML = "";
     for (const product of carrito) {
-        let {id,nombre,precio,cantidad,imagen} = product
+        const { id, nombre, precio, cantidad, imagen } = product
         if (cantidad > 0) {
             tabla.innerHTML += `<th><img src="./imagenes/${imagen}" class="card-img-top img-fluid" alt="${nombre}"></th>
                         <th>${nombre}</th>
@@ -105,6 +122,6 @@ btnCarrito.addEventListener("click", () => {
         }
     }
     //Si hay productos muestra el El precio total
-   mostrar(total)
+    mostrar(total)
     eliminarProducto(carrito)
 })
